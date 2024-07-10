a46ba5a0c66d2ab645c4973fea4f8d0ffc45192e
Nguyen Minh Tan <99700061+tantahi28@users.noreply.github.com>
2024-07-10 17:21:03 +0700

    wip

--- FE/app/nhatro/page.tsx
+++ FE/app/nhatro/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
     return <div className="">
             <p> breadcrums/nhatro</p>
             <div className="content-center mx-auto grid grid-cols-12 gap-5">
-                <div className="site-left p-4 bg-bg-secondary col-span-7 rounded">
+                <div className="site-left p-4 bg-secondary col-span-7 border border-gray-200 rounded">
                     <div className="flex justify-between">
                         <h2 className="font-bold">
                             Tổng 206 kết quả
@@ -10,6 +10,20 @@ export default function Page() {
                         <p>
                             Cập nhật: 13:17 04/03/2024
                         </p>
+                    </div>
+                    <div className="flex justify-start items-center my-4">
+                        <span>Sắp xếp: </span>
+                        <div className="">
+                            <form className="max-w-sm mx-auto">
+                                <select id="countries" className="rounded py-1 px-2">
+                                    <option selected>Mặc định</option>
+                                    <option value="US">United States</option>
+                                    <option value="CA">Canada</option>
+                                    <option value="FR">France</option>
+                                    <option value="DE">Germany</option>
+                                </select>
+                            </form>
+                        </div>
                     </div>
                 </div>
                 <div className="site-right p-4 col-span-5 rounded">
--- FE/tailwind.config.ts
+++ FE/tailwind.config.ts
@@ -8,12 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      gridTemplateColumns: {
-        '13': 'repeat(13, minmax(0, 1fr))',
-      },
       backgroundColor: {
-        'bg-color': 'rgba(245, 245, 245, 1)',
-        'bg-secondary': 'rgba(255, 255, 255, 1)'
+        'color': 'rgba(245, 245, 245, 1)',
+        'secondary': 'rgba(255, 255, 255, 1)'
       },
       fontSize: {
         base: '14px',
@@ -22,6 +19,7 @@ const config: Config = {
         'dark-blue': 'rgba(15, 33, 103, 1)',
         'medium-blue': 'rgba(53, 89, 224, 1)',
         'light-blue': 'rgba(76, 185, 231, 1)',
+        'border-color': 'rgba(217, 217, 217, 1)'
       },
     },
     keyframes: {
