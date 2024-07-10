f735274f521c80ebb3b0721da36a0c8eb3e47b4c
Nguyen Minh Tan <99700061+tantahi28@users.noreply.github.com>
2024-07-10 13:52:09 +0700

    initialStructure

---
+++ FE/app/layout.tsx
@@ -0,0 +1,22 @@
+import '@/app/ui/global.css';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+    title: {
+      template: '%s | CanThoHub',
+      default: 'CanThoHub',
+    },
+    description: 'The official Next.js Learn Dashboard built with App Router.',
+  };
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
