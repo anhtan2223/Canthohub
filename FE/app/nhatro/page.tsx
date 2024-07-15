3320f7dae309b0d6a58bb00f8364225a39675544
Nguyen Minh Tan <99700061+tantahi28@users.noreply.github.com>
2024-07-15 15:55:51 +0700

    commit

--- FE/app/nhatro/page.tsx
+++ FE/app/nhatro/page.tsx
@@ -1,7 +1,7 @@
 import { MdOutlineKeyboardArrowRight, MdKeyboardArrowLeft  } from "react-icons/md";
-import { FaChevronRight } from "react-icons/fa6";
 import Card from "@/app/ui/nhatro/card";
 import News from "@/app/ui/nhatro/news";
+import { FilterPrice, FilterArea } from "../ui/nhatro/filters";
 export default function Page() {
     return <div className="">
         <p className="my-4"> Trang chủ / <span className="text-medium-blue">Nhà trọ</span></p>
@@ -77,119 +77,9 @@ export default function Page() {
                 </div>
             </div>
             <div className="site-right col-span-4 rounded">
-                <div className="box-content container px-4 py-6 border border-border-color rounded-lg bg-secondary w-full rounded">
-                    <h2 className="font-bold text-lg">Tìm trọ ở khu vực</h2>
-                    <div className="grid grid-cols-2 mt-3 gap-4">
-                        <div className=" rounded">
-                            <span className="text-dark-blue font-semibold">
-                                Ninh kiều <span className="text-gray-400">(90)</span>
-                            </span>
-                        </div>
-                        <div className=" rounded">
-                            <span className="text-dark-blue font-semibold">
-                                Cái Răng <span className="text-gray-400">(90)</span>
-                            </span>
-                        </div>
-                    </div>
-                    <hr className="border-t-1 border-dashed border-gray-400 mt-3"></hr>
-                    <div className="grid grid-cols-2 gap-4 mt-3">
-                        <div className=" rounded">
-                            <span className="text-dark-blue font-semibold">
-                                Bình Thủy <span className="text-gray-400">(90)</span>
-                            </span>
-                        </div>
-                        <div className=" rounded">
-                            <span className="text-dark-blue font-semibold">
-                                Phong Điền <span className="text-gray-400">(90)</span>
-                            </span>
-                        </div>
-                    </div>
-                    <hr className="border-t-1 border-dashed border-gray-400 mt-3"></hr>
-                </div>
-                <div className="box-content container mt-3 px-4 py-6 border border-border-color rounded-lg bg-secondary w-full rounded">
-                    <h2 className="font-bold text-lg">Xem theo giá</h2>
-                    <div className="grid grid-cols-2 mt-3 gap-4">
-                        <div className=" rounded">
-                            <span className="flex items-center text-xs">
-                                <FaChevronRight className="text-gray-400 mr-2" size={12}/> Dưới 1 triệu
-                            </span>
-                        </div>
-                        <div className=" rounded">
-                            <span className="flex items-center text-xs">
-                                <FaChevronRight className="text-gray-400 mr-2" size={12}/> Dưới 1 triệu
-                            </span>
-                        </div>
-                    </div>
-                    <hr className="border-t-1 border-dashed border-gray-400 mt-3"></hr>
-                    <div className="grid grid-cols-2 mt-3 gap-4">
-                        <div className=" rounded">
-                            <span className="flex items-center text-xs">
-                                <FaChevronRight className="text-gray-400 mr-2" size={12}/> Dưới 1 triệu
-                            </span>
-                        </div>
-                        <div className=" rounded">
-                            <span className="flex items-center text-xs">
-                                <FaChevronRight className="text-gray-400 mr-2" size={12}/> Dưới 1 triệu
-                            </span>
-                        </div>
-                    </div>
-                    <hr className="border-t-1 border-dashed border-gray-400 mt-3"></hr>
-                    <div className="grid grid-cols-2 mt-3 gap-4">
-                        <div className=" rounded">
-                            <span className="flex items-center text-xs">
-                                <FaChevronRight className="text-gray-400 mr-2" size={12}/> Dưới 1 triệu
-                            </span>
-                        </div>
-                        <div className=" rounded">
-                            <span className="flex items-center text-xs">
-                                <FaChevronRight className="text-gray-400 mr-2" size={12}/> Dưới 1 triệu
-                            </span>
-                        </div>
-                    </div>
-                    <hr className="border-t-1 border-dashed border-gray-400 mt-3"></hr>
-                </div>
-                <div className="box-content container mt-3 px-4 py-6 border border-border-color rounded-lg bg-secondary w-full rounded">
-                    <h2 className="font-bold text-lg">Xem diện tích </h2>
-                    <div className="grid grid-cols-2 mt-3 gap-4">
-                        <div className=" rounded">
-                            <span className="flex items-center text-xs">
-                                <FaChevronRight className="text-gray-400 mr-2" size={12}/> Dưới 1 triệu
-                            </span>
-                        </div>
-                        <div className=" rounded">
-                            <span className="flex items-center text-xs">
-                                <FaChevronRight className="text-gray-400 mr-2" size={12}/> Dưới 1 triệu
-                            </span>
-                        </div>
-                    </div>
-                    <hr className="border-t-1 border-dashed border-gray-400 mt-3"></hr>
-                    <div className="grid grid-cols-2 mt-3 gap-4">
-                        <div className=" rounded">
-                            <span className="flex items-center text-xs">
-                                <FaChevronRight className="text-gray-400 mr-2" size={12}/> Dưới 1 triệu
-                            </span>
-                        </div>
-                        <div className=" rounded">
-                            <span className="flex items-center text-xs">
-                                <FaChevronRight className="text-gray-400 mr-2" size={12}/> Dưới 1 triệu
-                            </span>
-                        </div>
-                    </div>
-                    <hr className="border-t-1 border-dashed border-gray-400 mt-3"></hr>
-                    <div className="grid grid-cols-2 mt-3 gap-4">
-                        <div className=" rounded">
-                            <span className="flex items-center text-xs">
-                                <FaChevronRight className="text-gray-400 mr-2" size={12}/> Dưới 1 triệu
-                            </span>
-                        </div>
-                        <div className=" rounded">
-                            <span className="flex items-center text-xs">
-                                <FaChevronRight className="text-gray-400 mr-2" size={12}/> Dưới 1 triệu
-                            </span>
-                        </div>
-                    </div>
-                    <hr className="border-t-1 border-dashed border-gray-400 mt-3"></hr>
-                </div>
+
+                <FilterPrice/>
+                <FilterArea/>
                 <News/>
             </div>
         </div>
