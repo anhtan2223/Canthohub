19e0f433978b18dbadffbd93eff8f343ee6c3128
HuynhNhutHoa <hoab2005755@student.ctu.deu.vn>
2024-07-16 08:50:17 +0700

    News Detail

--- FE/app/tintuc/[id]/page.tsx
+++ FE/app/tintuc/[id]/page.tsx
@@ -1,3 +1,26 @@
+import Events from "../../ui/tintuc/events";
+import EventsSection from "../../ui/tintuc/EventsSection";
+import NewsDetail from "../../ui/tintuc/NewsDetail";
+import RelatedNews from "../../ui/tintuc/RelatedNews";
+
 export default function Page() {
-    return <p>Trang chi tiết tin tức</p>;
+    // return <p>Trang chi tiết tin tức</p>;
+    return (
+        <div className="flex flex-col items-center space-y-6">
+            <div className="w-full">
+                <img src="/welcome.png" alt="" className='w-full' />
+                <p className="mt-4"> Trang chủ / <span className="text-medium-blue">Tin tức</span></p>
+            </div>
+            <div className="flex space-x-6 justify-center items-start w-full">
+                <div className="flex-grow">
+                    <NewsDetail />
+                    <RelatedNews />
+                </div>
+                <div className="flex flex-col space-y-6 flex-shrink-0" style={{ width: '280px' }}>
+                    <Events />
+                    <EventsSection />
+                </div>
+            </div>
+        </div>
+    );
 }
---
+++ FE/app/ui/tintuc/NewsDetail.tsx
@@ -0,0 +1,82 @@
+'use client';
+
+import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+const NewsDetail = () => {
+    const post = {
+        id: 1,
+        author: 'Lan Lan',
+        title: 'Tuyển dụng Flutter tại Việt Nam',
+        time: '15:30 04/03/2024',
+        description: [
+            'Sẽ là thật tuyệt nếu rơi vào một chiều mưa, em ôm anh thật chặt và hỏi em tìm được việc chưa. Nếu chưa thì về team Avada Group nha, chúng mình đang tìm kiếm đồng đội Full Stack Nodejs + Reactjs.',
+            'Thu nhập: 12 - 25 triệu',
+            'Yêu cầu:',
+            '- Trên 1-2 năm kinh nghiệm làm việc với NodeJS và ReactJS',
+            '- Có kinh nghiệm làm việc với mô hình Agile; làm việc với Database NoSQL: MongoDB hoặc Firestore',
+            '- Có kinh nghiệm với điện toán đám mây, một trong: Google Cloud, Firebase, AWS, kiến trúc hệ thống microservice',
+            '- Khả năng đọc hiểu Tiếng Anh tốt, độ tuổi từ 23 - 30.',
+            'Quyền lợi:',
+            '- Được làm việc với những công nghệ hot nhất',
+            '- Tham gia xây dựng hệ thống lớn',
+            '- Được cung cấp thiết bị làm việc đầy đủ, hiện đại',
+            '- Được hưởng đầy đủ chế độ phúc lợi như BHXH, thưởng lễ Tết, lương tháng 13, du lịch hàng năm...',
+            '- Phụ cấp gửi xe, phụ cấp đi lại,...',
+            '- Văn phòng làm việc tiện nghi; cung cấp trà, cafe và có tủ đồ ăn uống, bánh kẹo hàng ngày',
+            '- Tham gia các CLB chạy, đá bóng, boardgame, văn nghệ ...',
+            '- Nghỉ T7, CN và nhiều chế độ cực hấp dẫn.',
+            'Địa chỉ làm việc: Trần Phú, Hà Đông, Hà Nội',
+            'Ứng tuyển ngay: Gửi CV vào mail anhvn@avadagroup.com (Tiêu đề ghi rõ NodeJS & ReactJS Developer - Họ và tên)'
+        ],
+        likes: '830k',
+        comments: '390k'
+    };
+
+    return (
+        <div className="bg-white px-6 pt-6 pb-10 rounded-md space-y-6 flex-grow">
+            <div key={post.id} className="bg-white p-4 rounded-md flex justify-between items-start border border-gray-200">
+                <div className="flex-1 mr-4">
+                    <div className="flex items-center space-x-2 mb-3">
+                        <div className="w-[54px] h-[54px] rounded-full flex items-center justify-center">
+                            <img src="/avatar.jpg" alt="Avatar" className="rounded-full w-full h-full" />
+                        </div>
+                        <div>
+                            <div className="font-bold border-r border-[#898989] inline-block pr-2 text-sm">{post.author}</div>
+                            <div className="font-bold inline-block p-2 text-sm">{post.title}</div>
+                            <div className="italic text-xs text-[#231F20]">{post.time}</div>
+                        </div>
+                    </div>
+                    <div className="text-sm text-black">
+                        {post.description.map((desc, i) => (
+                            <p key={i} className='pl-2'>&bull; {desc}</p>
+                        ))}
+                        <div className="flex-shrink-0 w-[541px] h-[454px] mt-4">
+                            <img src="/avadagroup.png" alt="Food" className="w-full h-auto object-cover rounded-md" />
+                        </div>
+                    </div>
+                    <div className="mt-9 mb-9">
+                        <a
+                            href="#"
+                            className="flex items-center text-xl h-12 w-56 text-blue-500 border border-black-500 rounded-md px-2 py-1"
+                        >
+                            Xem trên facebook <FiArrowRight className="ml-1 text-black" />
+                        </a>
+                    </div>
+                    <div className="mt-2 text-gray-500 text-xs flex space-x-4">
+                        <div className="flex items-center space-x-1">
+                            <span>{post.likes}</span>
+                            <img src="/Icon/like.png" alt="" className='w-[21px] h-[21px]' />
+                        </div>
+                        <div className="flex items-center space-x-1">
+                            <span>{post.comments}</span>
+                            <img src="/Icon/cmt.png" alt="" className='w-[21px] h-[21px]' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default NewsDetail;
